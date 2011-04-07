9433f92ba7b56ace356eab712b99133ca30d8e50
lord_julian@gmx.de <lord_julian@gmx.de@0942d757-a625-f9e6-5778-5b17af3a72c8>
2011-04-07 09:50:11 +0000

    jaaa alles klar

---
+++ trunk/bavarian-hightech-banking/src/bavaria/hightech/banking/Money.java
@@ -0,0 +1,142 @@
+package bavaria.hightech.banking;
+
+public class Money {
+	private long value;
+	private Currency currency;
+
+	public Money(long value, Currency currency) {
+		this.setValue(value);
+		this.setCurrency(currency);
+	}
+
+	public void setValue(long value) {
+		this.value = value;
+	}
+
+	public long getValue() {
+		return value;
+	}
+
+	public void setCurrency(Currency currency) {
+		switch (currency) {
+		case EURO:
+			this.currency = Currency.EURO;
+			break;
+		case USDOLLAR:
+			this.currency = Currency.USDOLLAR;
+			break;
+		case SCHWEIZERFRANKEN:
+			this.currency = Currency.SCHWEIZERFRANKEN;
+			break;
+		case JAPANISCHERYEN:
+			this.currency = Currency.JAPANISCHERYEN;
+			break;
+		case BRITISCHESPFUND:
+			this.currency = Currency.BRITISCHESPFUND;
+			break;
+		}
+	}
+
+	public Currency getCurrency() {
+		return currency;
+	}
+
+	public enum Currency {
+		EURO(1.0000), USDOLLAR(0.7000), SCHWEIZERFRANKEN(0.7598), JAPANISCHERYEN(
+				0.0082), BRITISCHESPFUND(1.1419);
+
+		private double factor;
+
+		public void setFactor(double factor) {
+			this.factor = factor;
+		}
+
+		public double getFactor() {
+			return factor;
+		}
+
+		Currency(double factor) {
+			this.factor = factor;
+		}
+	}
+
+	public void add(long value, Currency currency) {
+		switch (currency) {
+		case EURO:
+			this.value = (long) (this.value + (value * Currency.EURO
+					.getFactor()) / this.currency.getFactor());
+			break;
+		case USDOLLAR:
+			this.value = (long) (this.value + (value * Currency.USDOLLAR
+					.getFactor()) / this.currency.getFactor());
+			break;
+		case SCHWEIZERFRANKEN:
+			this.value = (long) (this.value + (value * Currency.SCHWEIZERFRANKEN
+					.getFactor()) / this.currency.getFactor());
+			break;
+		case JAPANISCHERYEN:
+			this.value = (long) (this.value + (value * Currency.JAPANISCHERYEN
+					.getFactor()) / this.currency.getFactor());
+			break;
+		case BRITISCHESPFUND:
+			this.value = (long) (this.value + (value * Currency.BRITISCHESPFUND
+					.getFactor()) / this.currency.getFactor());
+			break;
+		}
+	}
+
+	public void sub(long value, Currency currency) {
+		switch (currency) {
+		case EURO:
+			this.value = (long) (this.value - (value * Currency.EURO
+					.getFactor()) / this.currency.getFactor());
+			break;
+		case USDOLLAR:
+			this.value = (long) (this.value - (value * Currency.USDOLLAR
+					.getFactor()) / this.currency.getFactor());
+			break;
+		case SCHWEIZERFRANKEN:
+			this.value = (long) (this.value - (value * Currency.SCHWEIZERFRANKEN
+					.getFactor()) / this.currency.getFactor());
+			break;
+		case JAPANISCHERYEN:
+			this.value = (long) (this.value - (value * Currency.JAPANISCHERYEN
+					.getFactor()) / this.currency.getFactor());
+			break;
+		case BRITISCHESPFUND:
+			this.value = (long) (this.value - (value * Currency.BRITISCHESPFUND
+					.getFactor()) / this.currency.getFactor());
+			break;
+		}
+	}
+
+	public boolean equal(long value) {
+		if (this.value == value) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean bigger(long value) {
+		if (this.value > value) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean smaler(long value) {
+		if (this.value < value) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void changeCurrency(Currency currency) {
+		value = (long) ((value * this.currency.getFactor()) / currency
+				.getFactor());
+		setCurrency(currency);
+	}
+}
